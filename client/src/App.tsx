4d29026e7b9c9828758df640b1b94d456f84273e
Developer <dev@poi-gis.local>
2026-06-12 19:29:47 +0800

    feat: project scaffold - monorepo, server, client

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <h1>POI 数据采集工具</h1>
+    </div>
+  );
+}
+
+export default App;
